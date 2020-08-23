e8f90843d90ad9a4b306d9718f0b051cc962b8e4
Efe <knowledgeagare157@gmail.com>
2020-08-23 22:55:03 +0100

    add new port to config

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const app = express();
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -15,3 +16,4 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
+// Access-Control-Allow-Credentials: true
--- src/config/system.js
+++ src/config/system.js
@@ -1,6 +1,6 @@
 const system = {
-    appName: 'SHOPMATE',
-    serverBaseUrl: 'https://backendapi.turing.com',
+    appName: `Efe's-Mart`,
+    serverBaseUrl: 'http://localhost:8000',
     imageBaseUrl: 'https://backendapi.turing.com/images/products/',
     stripeToken: 'pk_test_NcwpaplBCuTL6I0THD44heRe'
 };
